a719a715c2e1099dea6c4a1cbc88fe6dea3d8a05
joowan <bust3902@gmail.com>
2022-11-12 14:13:43 +0900

    리팩토링 4 완료.
    라우터 내비게이션 가드 적용.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import NewsView from '../views/NewsView.vue'
 import AskView from '../views/AskView.vue'
 import JobsView from '../views/JobsView.vue'
 import ItemView from '../views/ItemView.vue'
+import bus from "../utils/eventBus";
+import { store } from '../store/index'
 // import createListView from '../views/CreateListView'
 
 Vue.use(VueRouter);
@@ -21,19 +23,54 @@ export const router = new VueRouter({
             path: '/news',
             name: 'news',
             // component: url 주소로 갔을 때 표시 될 컴포넌트
-            component: NewsView
+            component: NewsView,
             // component: createListView('NewsView')
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        console.log('fetched');
+                        bus.$emit('end:spinner')
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                // console.log('to', to);
+                // console.log('from', from);
+                // console.log('next', next);
+            }
         },
         {
             path: '/ask',
             name: 'ask',
-            component: AskView
+            component: AskView,
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        console.log('fetched');
+                        bus.$emit('end:spinner');
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            }
             // component: createListView('AskView')
         },
         {
             path: '/jobs',
             name: 'jobs',
-            component: JobsView
+            component: JobsView,
+            beforeEnter: (to, from, next) => {
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => next())
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            }
             // component: createListView('JobsView')
         },
         {
