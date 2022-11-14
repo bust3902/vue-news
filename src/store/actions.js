df9a9b8fa76bca27084c95adfe4cf8ca3db54bf5
joowan <bust3902@gmail.com>
2022-11-14 13:42:10 +0900

    리팩토링 5 완료.
    async & await 적용.

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,6 +8,8 @@ import {
 } from "@/api";
 
 export default {
+    // promise
+    /*
     FETCH_NEWS(context) {
         fetchNewsList()
             .then(response => {
@@ -16,38 +18,42 @@ export default {
             })
             .catch(error => console.log(error))
     },
-    FETCH_ASK(context) {
-        fetchAskList()
-            .then(response => context.commit('SET_ASK', response.data))
-            .catch(error => console.log(error))
+     */// async
+    async FETCH_NEWS(context) {
+        const response = await fetchNewsList();
+        context.commit('SET_NEWS', response.data);
+        return response;
     },
     // FETCH_JOBS({ commit })는 context.commit과 같다. commit을 바로 받아서 사용.
     // 마찬가지로 then에서 response의 data를 바로 받아 사용.
-    FETCH_JOBS({ commit }) {
-        fetchJobsList()
-            .then(({ data }) => commit('SET_JOBS', data))
-            .catch(error => console.log(error))
+    async FETCH_JOBS({ commit }) {
+        try {
+            const response = await fetchJobsList();
+            commit('SET_JOBS', response.data);
+            return response;
+        } catch (error) {
+            console.log(error)
+        }
     },
-    FETCH_USER({ commit }, name) {
-        return fetchUserInfo(name)
-            .then(({ data }) => {
-                commit('SET_USER', data)
-            })
-            .catch(error => console.log(error));
+    async FETCH_ASK({ commit }) {
+        const response = await fetchAskList()
+        commit('SET_ASK', response.data);
+        return response;
     },
-    FETCH_ITEM({ commit }, id) {
-        return fetchCommentItem(id)
-            .then(({ data }) => {
-                commit('SET_ITEM', data);
-            })
-            .catch(error => console.log(error));
+    async FETCH_USER({ commit }, name) {
+        const response = await fetchUserInfo(name);
+        commit('SET_USER', response.data);
+        return response;
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-            .then(response => {
-                commit('SET_LIST', response.data);
-                return response;
-            })
-            .catch(error => console.log(error));
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchCommentItem(id);
+        commit('SET_ITEM', response.data);
+        return response;
+    },
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName);
+        console.log(4);
+        commit('SET_LIST', response.data);
+        return response;
     }
 }
